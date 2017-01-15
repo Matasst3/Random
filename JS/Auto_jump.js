0dfc55f67a2826a10e19f9a0c71cb3460ef92e0f
Matasst3 <matasst3@gmail.com>
2017-01-15 16:33:44 +0200

    Add files via upload

---
+++ JS/Auto_jump.js
@@ -0,0 +1,16 @@
+﻿ var distToGround: float;
+ var jumpSpeed: float = 8;
+ 
+ function Start(){
+   distToGround = GetComponent.<Collider>().bounds.extents.y;
+ }
+ 
+ function IsGrounded(): boolean {
+   return Physics.Raycast(transform.position, -Vector3.up, 0.07);
+ }
+ 
+ function Update () {
+   if (IsGrounded()){
+     GetComponent.<Rigidbody>().velocity.y = jumpSpeed;
+   }
+ }
